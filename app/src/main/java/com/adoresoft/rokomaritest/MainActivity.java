fd638ad314742d6bc18827f0a7ebb1baab345237
arifur-rafid <arifur.rafid@gmail.com>
2021-11-06 15:56:00 +0600

    First commit

---
+++ app/src/main/java/com/adoresoft/rokomaritest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.adoresoft.rokomaritest;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    public BottomNavigationView navigation;
+    public NavController navController;
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
